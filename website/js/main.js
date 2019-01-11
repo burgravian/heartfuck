0b6944894410d4414cf00af220cdab1bc664bcb5
redshifts <laura.pircalaboiu@yahoo.it>
2019-01-11 13:05:19 +0100

    minor stuff

--- website/js/main.js
+++ website/js/main.js
@@ -290,11 +290,5 @@ function interpretHeartfuck() {
 }
 
 function makeid() {
-  var text = "";
-  var possible = "abcdefghijklmnopqrstuvwxyz";
-
-  for (var i = 0; i < 5; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
+  console.log("yeet");
 }
